a7342cb55b8de7fadcf1aaba518356e8a5415b6d
melkit235 <verghel.iliya@gmail.com>
2021-12-13 00:56:55 +0300

    lab2

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <math.h>
+
+#define pi 3.14159265
+#define e 2,7182818284
+
+using std::cin;
+using std::cout;
+
+double fv2=0;
+
+int main()
+{
+	for (double x = 0.6; x <= 1.1; x+=0.25)
+	{
+		double fv1 = sqrt(pi / 2 + x);
+		for (int n = 1; n <= 15; n++)
+		{
+			if (n-x>0)
+			{
+				fv2 = fv2 + (exp(1) / (pow(n, 2) + x) * exp(log(n - x) / 3));
+			}
+			else
+			{
+				fv2 = fv2 - (exp(1) / (pow(n, 2) + x) * exp(log(x - n) / 3));;
+			}
+			if (n>9)
+			{
+				cout << "x=" << x << " n=" << n << " f=" << fv1*fv2 << '\n';
+			}
+		}
+		fv2 = 0;
+	}
+}
